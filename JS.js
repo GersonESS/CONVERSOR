88b2429e3d0ecc8598baeb78301c854cb9dc3abe
Gerson Santos <gersones@outlook.com>
2021-04-08 14:58:19 -0300

    a

---
+++ JS.js
@@ -0,0 +1,10 @@
+alert("GersonES")
+var nome = "GersonES"
+var idade = 64;
+idade = idade + 1
+alert(nome + idade)
+var  ValorDolarTxt = prompt("Valor:")
+varlorDularNum = parseInt(ValorDolarTxt)
+ValorReal = varlorDularNum * 5.51
+ValorRealDec = ValorReal.toFixed(2)
+alert(ValorRealDec);
